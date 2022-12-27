7512eddc5eddf82ba8cba62bd1efa566ed5f606d
SergioCortesR <sergio.cortes@cceo.com.mx>
2022-12-27 17:29:41 -0600

    perf :rocket: The Category CRUD was added to the laravel-app

---
+++ laravel-crud/app/Http/Requests/Category/StoreRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests\Category;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Str;
+
+class StoreRequest extends FormRequest
+{
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            //'slug' => Str::slug($this->title)
+            'slug' => str($this->title)->slug() 
+        ]);
+    }
+    static public function myRules()
+    {
+        return [
+            "title" => "required|min:5|max:500",
+            "slug" => "required|min:5|max:500|unique:posts"
+        ];
+    }
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        //Reglas de validacion para poder hacer un insert en la base de datos
+        return $this->myRules();
+    }
+}
